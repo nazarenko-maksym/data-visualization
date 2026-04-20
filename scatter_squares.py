f6b4aa1887d4753427cccb93cbde62b573803043
nazarenko-maksym <mrnazarenkom@gmail.com>
2026-04-20 15:28:28 -0400

    plot and style points with scatter()

---
+++ scatter_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+plt.style.use('seaborn-v0_8')
+fix, ax = plt.subplots()
+ax.scatter(2, 4, s=200)
+
+# Set chart title and label axes
+ax.set_title("Square Numbers", fontsize=14)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(labelsize=14)
+
+plt.show()
