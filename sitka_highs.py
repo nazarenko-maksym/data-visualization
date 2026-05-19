d7f116729c396dda0028055892be01f4c4299527
nazarenko-maksym <mrnazarenkom@gmail.com>
2026-05-19 14:55:56 -0400

    plot a larger timeframe

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -25,7 +25,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
 
 # Format plot
-ax.set_title('Daily High Temperatures, July 2021', fontsize=24)
+ax.set_title('Daily High Temperatures, 2021', fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperatures (F)', fontsize=16)
